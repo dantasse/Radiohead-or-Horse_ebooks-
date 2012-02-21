4b08529ab487dba3e612cc03261a103fe7ea3567
Dan Tasse <dan.tasse@gmail.com>
2012-02-21 15:17:59 +1300

    app engine and templates hello world

---
+++ rohe.py
@@ -0,0 +1,27 @@
+import os
+
+# http://code.google.com/appengine/docs/python/tools/libraries.html#Django 
+from google.appengine.dist import use_library
+use_library('django', '1.2')
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        template_values = {
+            'name': 'dan'
+        }
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, template_values))
+
+application = webapp.WSGIApplication(
+                                     [('/', MainPage)],
+                                     debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
