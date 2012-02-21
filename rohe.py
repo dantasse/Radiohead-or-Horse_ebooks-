7f615c2c7785cc5943a6653555d21db118d05ed2
Dan Tasse <dan.tasse@gmail.com>
2012-02-21 18:28:36 +1300

    moving lib files around I guess

--- rohe.py
+++ rohe.py
@@ -4,13 +4,24 @@ import os
 from google.appengine.dist import use_library
 use_library('django', '1.2')
 
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "lib/"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "lib/python-oauth2"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "lib/python-twitter"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "lib/httplib2"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "lib/simplejson"))
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
+import twitter
 
 class MainPage(webapp.RequestHandler):
     def get(self):
+        api = twitter.Api()
+        statuses = api.GetUserTimeline('Horse_ebooks')
         template_values = {
+            'tweets': [s.text for s in statuses]
         }
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path, template_values))
